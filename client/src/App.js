f331ed1c1297e3c9af4cdb7f0c555038c185ee41
valentine <valentine.bott@infinityworks.com>
2022-07-07 11:48:26 +0100

    basic react setup with example lambda to database call and listing
    Signed-off-by: valentine <valentine.bott@infinityworks.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+    const [text, setText] = useState([])
+    const lambdaUrl = "https://a2pp63z27yhzgudqlxshixm3240ctyjj.lambda-url.eu-west-2.on.aws/"
+
+    useEffect(() => {
+        fetch(lambdaUrl)
+            .then(res => res.json())
+            .then(data => data.rows)
+            .then(setText)
+    }, [])
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>Everyone in the database...</p>
+                {text.map((el, i) => {
+                    return (<p key={i}>{el.user_name}</p>)
+                })}
+                <br />
+                <p>Everyone in team NHS-1</p>
+                {text.filter(user => user.team_id === 'NHS-1').map((el, i) => {
+                    return (<p key={i}>{el.user_name}</p>)
+                })}
             </header>
         </div>
     );
