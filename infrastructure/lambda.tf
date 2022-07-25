0336f625240ba924b252be24a412af2040e158ab
valentine <valentine.bott@infinityworks.com>
2022-07-25 11:29:02 +0100

    added new lambda
    Signed-off-by: valentine <valentine.bott@infinityworks.com>

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -6,7 +6,7 @@ module "postgres" {
   source = "./modules/db"
 }
 
-module "function" {
+module "getTeam" {
   source = "./modules/lambda"
 
   network_configuration = module.network.network_config
@@ -22,3 +22,18 @@ module "function" {
   }
 }
 
+module "getUserDetails" {
+  source = "./modules/lambda"
+
+  network_configuration = module.network.network_config
+  name                  = "getUserDetails"
+  memory                = 512
+  timeout_seconds       = 5 * 60
+  environment = {
+    DB_HOST     = module.postgres.host
+    DB_USERNAME = module.postgres.username
+    DB_PASSWORD = module.postgres.db_pass
+    DB_NAME     = module.postgres.db_name
+    DB_PORT     = module.postgres.port
+  }
+}
